6ae5649b63bc65d2a2d3654f35e8bf21b3880671
Safet Purkovic <sajo_sj@hotmail.com>
2025-01-18 09:30:38 +0100

    aa

--- app.js
+++ app.js
@@ -1,9 +1,15 @@
 const express = require('express');
+var cors = require('cors')
 const app = express();
 const port = process.env.PORT || 4000;
 const vebDizajnController = require('./vebDizajnController');
-
-
+var defaultCors = {
+    "origin": "*",
+    "methods": "GET,HEAD,PUT,PATCH,POST,DELETE",
+    "preflightContinue": false,
+    "optionsSuccessStatus": 204
+  }
+app.use(cors(defaultCors));
 // Middleware to parse JSON requests
 app.use(express.json());
 app.use('/api/vebDizajn', vebDizajnController);
