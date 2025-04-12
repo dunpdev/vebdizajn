4c8742f639c325847e9b996aa43beabfd1d04551
vsdesignsjenica <32780491+vsdesignsjenica@users.noreply.github.com>
2025-04-12 16:43:51 +0200

    Update vebDizajnController.js

--- vebDizajnController.js
+++ vebDizajnController.js
@@ -6480,7 +6480,7 @@ router.get('/vak', (req, res) => {
   ezanE.setMinutes(ezanE.getMinutes() - 40);
   let resp = {
     sabah: `5:15`,
-    sabahS: `5:30`,
+    sabahS: `5:15`,
     sabahN: `5:15`,
     sabahC: `${ezan.getHours()}:${ezan.getMinutes().toString().padStart(2, "0")}`,
     sabahE: `${ezanE.getHours()}:${ezanE.getMinutes().toString().padStart(2, "0")}`,
