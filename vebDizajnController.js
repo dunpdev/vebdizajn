05db91830b46d5732f8df3af0dfe1e65f9856997
vsdesignsjenica <32780491+vsdesignsjenica@users.noreply.github.com>
2025-12-24 08:24:39 +0100

    Update sabahA time from 6:15 to 6:30

--- vebDizajnController.js
+++ vebDizajnController.js
@@ -6832,7 +6832,7 @@ router.get('/vak', (req, res) => {
     sabahS: `6:15`,
     sabahN: `6:30`,
     sabahV: `6:15`,
-    sabahA: `6:15`,
+    sabahA: `6:30`,
     hadisi: ["\"Ne postoje dani u kojima je činjenje dobrih djela draže Allahu od ovih dana\", misleći na prvih deset dana tog mjeseca. Rekli su ashabi:\"Čak ni borba na Allahovom putu?\" Rekao je:\"Ni borba na Allahovom putu!\" A zatim je nastavio:\"Osim čovjeka koji izađe u borbu na Allahovom putu boreći se svojim tijelom i imetkom, i ne vrati ništa od toga.\" (Buharija);\"Ne postoje veličanstveniji dani kod Allaha, niti dani u kojima su dobra djela draža Allahu od ovih deset dana. Mnogo u njima Allaha spominjite et-tehlilom (izgovarajući riječi la ilahe illallah), et-tekbirom (veličanjem Allaha izgovarajući riječi Allahu ekber), et-tehmidom (zahvalom Allahu riječima elhamdulillah.)\"; Kazao je Allahov Poslanik, kada je bio upitan o postu na Dan Arefata (devetom danu zul-hidždžeta):\"Iskupljuje od grijeha prošlu i narednu godinu.\"(Muslim i dr.)"],
     sabahC: `${ezan.getHours()}:${ezan.getMinutes().toString().padStart(2, "0")}`,
     sabahE: `${ezanE.getHours()}:${ezanE.getMinutes().toString().padStart(2, "0")}`,
