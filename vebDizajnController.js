7f0b1b0f7a0473c61d4c0d8cd0f9b126a303dfcf
vsdesignsjenica <32780491+vsdesignsjenica@users.noreply.github.com>
2026-09-14 05:50:09 +0200

    Update sabah time from 5:15 to 5:30

--- vebDizajnController.js
+++ vebDizajnController.js
@@ -6919,7 +6919,7 @@ router.get('/vak', (req, res) => {
   // add 5 minutes to ezan
   ezanE.setMinutes(ezanE.getMinutes() - 40);
   let resp = {
-    sabah: `5:15`,
+    sabah: `5:30`,
     sabahEH: `5:00`,
     sabahN: `5:30`,
     sabahV: `5:30`,
